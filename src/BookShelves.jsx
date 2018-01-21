4a313f13d2ee19c3887fa1b6920ef7d53086bd65
Thiago Abreu <thiago.abreu@gmail.com>
2018-01-21 17:28:09 -0200

    Comentando metodo sortBy
    Esse método foi comentado para fazer a submissão do projeto no Udacity.
     Pretendo voltar a usa-lo no proximo commit

--- src/BookShelves.jsx
+++ src/BookShelves.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import escapeRegExp from 'escape-string-regexp'
-//import sortBy from 'sort-by'
 import PropTypes from 'prop-types'
 
 import Bookshelf from './BookShelf'
